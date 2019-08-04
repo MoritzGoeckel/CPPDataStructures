e75bbda59613193ca9975ab35aacf798f8a8a249
Moritz <moritz.goeckel@gmail.com>
2019-08-04 15:29:02 +0000

    add iterator to list

--- src/list/list.hpp
+++ src/list/list.hpp
@@ -1,15 +1,12 @@
 #ifndef LIST_H
 #define LIST_H
 
-// TODO: Need iterator
-
 namespace mtl{
 
 template<typename T>
 class List{
 
     private:
-
     struct Entry{
         T value;
         Entry* next;
@@ -29,10 +26,53 @@ class List{
     size_t _size;
 
     public:
+    class Iterator{
+        friend List;
+
+        private:
+        Entry* _ptr;
+
+        Iterator(Entry* ptr) : _ptr(ptr){ }
+
+        public:
+        T& operator*() { return _ptr->value; }
+
+        Iterator operator++(){
+            _ptr = _ptr->next;
+            return *this;
+        }
+
+        bool operator==(const Iterator& rhs) const noexcept {
+            return rhs._ptr == _ptr;
+        }
+
+        bool operator!=(const Iterator& rhs) const noexcept {
+            return !operator==(rhs);
+        }
+
+        T* operator->() const { return &(_ptr->value); }
+    };
+
     List() : _begin(nullptr), _end(nullptr), _size(0) { }
 
     size_t size() const noexcept {
         return _size;
+    }
+
+    Iterator begin() const noexcept {
+        return Iterator(_begin);
+    }
+
+    Iterator end() const noexcept {
+        return Iterator(nullptr);
+    }
+
+    Iterator find(T value){
+        for(auto it = begin(); it != end(); ++it){
+            if(*it == value)
+                return it;
+        }
+        return Iterator(nullptr);
     }
 
     void push_front(T value){
@@ -61,9 +101,9 @@ class List{
         _size++;
     }
 
-    T operator[](size_t index) const { return at(index); }
+    T& operator[](size_t index) { return at(index); }
 
-    T at(size_t index) const { return entryAt(index)->value; }
+    T& at(size_t index) { return entryAt(index)->value; }
 
     T front() const {
         if(_size == 0)
@@ -79,7 +119,7 @@ class List{
         return _end->value;
     }
 
-    void remove(size_t index){
+    void remove(size_t index){ // TODO: with iterator
         if(index > _size)
             throw "Index out of bounds";
 
@@ -103,7 +143,7 @@ class List{
             _end = before;
     }
 
-    void insert(size_t index, T value){
+    void insert(size_t index, T value){ // TODO: with iterator
         if(index > _size)
             throw "Index out of bounds";
 
@@ -118,7 +158,7 @@ class List{
         before->next = new Entry(value, before->next);
     }
 
-    ~List(){
+    void clear(){
         if(_begin == nullptr)
            return;
 
@@ -126,8 +166,12 @@ class List{
         delete _begin;
     }
 
+    ~List(){
+        clear();
+    }
+
     private:
-    Entry* entryAt(size_t index) const {
+    Entry* entryAt(size_t index) {
         if(index >= size())
             throw "index out of bounds";
 
--- src/list/tests.hpp
+++ src/list/tests.hpp
@@ -24,3 +24,24 @@ TEST(List, Basic) {
     ASSERT_EQ(14, list.front());
     ASSERT_EQ(18, list.back());
 }
+
+TEST(List, Iter){
+    mtl::List<int> list;
+    list.push_back(10);
+    list.push_back(11);
+    list.push_back(12);
+    list.push_back(13);
+
+    auto it = list.begin();
+    EXPECT_EQ(*it, 10);
+    EXPECT_EQ(false, it == list.end());
+
+    ++it;
+    EXPECT_EQ(*it, 11);
+
+    int i = 0;
+    for(auto it = list.begin(); it != list.end(); ++it){
+        EXPECT_EQ(*it, 10 + i);
+        i++;
+    }
+}
